fb0713c2febcd0b1311046a02c9c592820a569cc
egouge <devnull@localhost>
2008-10-29 20:48:55 +0100

    [svn r30943] updates to make the overview map location box movable

--- tutorials/net.refractions.udig.tutorials.rcp/src/net/refractions/udig/tutorials/rcp/OverviewMapViewer.java
+++ tutorials/net.refractions.udig.tutorials.rcp/src/net/refractions/udig/tutorials/rcp/OverviewMapViewer.java
@@ -13,17 +13,23 @@ import net.refractions.udig.project.render.displayAdapter.IMapDisplayListener;
 import net.refractions.udig.project.render.displayAdapter.MapDisplayEvent;
 import net.refractions.udig.project.ui.commands.AbstractDrawCommand;
 import net.refractions.udig.project.ui.commands.IDrawCommand;
+import net.refractions.udig.project.ui.render.displayAdapter.MapMouseEvent;
+import net.refractions.udig.project.ui.render.displayAdapter.ViewportPane;
+import net.refractions.udig.project.ui.tool.AbstractModalTool;
 import net.refractions.udig.project.ui.viewers.MapViewer;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
 import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 import com.vividsolutions.jts.geom.Coordinate;
+
 
 /**
  * An overview map viewer.
@@ -63,7 +69,7 @@ public class OverviewMapViewer {
      */
     public void setMap(Map map){
         if (mainmap.getViewportModel() == null){
-            throw new IllegalStateException("Cannot set the map until the main map has a viewport model.");
+            throw new IllegalStateException("Cannot set the map until the main map has a viewport model."); //$NON-NLS-1$
         }
         mapviewer.setMap(map);
         addContextListener();
@@ -85,7 +91,7 @@ public class OverviewMapViewer {
      * Creates a location box - which tracks the viewport of the main map
      * in the overview window.
      */
-    public void createLocationBox(){
+    public void createLocationBox(ViewportPane mainMapViewportPane){
         final Map overviewMap = this.mapviewer.getMap();
         rectDrawCommand = new AbstractDrawCommand(){
 
@@ -124,7 +130,8 @@ public class OverviewMapViewer {
             
         };
         mapviewer.getViewport().addDrawCommand(rectDrawCommand);
-        
+        //add tool to the location box
+        mapviewer.setModalTool(new LocationBoxMoveTool( mainMapViewportPane ));
     }
     
     /**
@@ -167,7 +174,6 @@ public class OverviewMapViewer {
             }
            
             private void updateMapAndRefresh(Notification msg){
-                System.out.println("added");
                 overviewmap.getContextModel().eNotify(msg);
                 overviewmap.getViewportModelInternal().setBounds(mainmap.getBounds(new NullProgressMonitor()));                
             }
@@ -232,5 +238,100 @@ public class OverviewMapViewer {
         }
         removeLocationBox();
     }
+    
+    
+    
+    /**
+     * Tool to apply to the overview map viewer to 
+     * allow the viewbox to be moved around and
+     * move the main box as well.
+     *    
+     *
+     * @author Emily Gouge
+     * @since 1.2.0
+     */
+    class LocationBoxMoveTool extends AbstractModalTool{
+        ViewportPane mainMapVP;
 
+        public LocationBoxMoveTool( ViewportPane mainMapVP){
+            super(MOUSE | MOTION);
+            this.mainMapVP = mainMapVP;
+
+        }
+        
+        private boolean dragging = false;
+        private org.eclipse.swt.graphics.Point startp = null;
+        
+        /**
+         * @see net.refractions.udig.project.ui.tool.AbstractTool#mouseDragged(net.refractions.udig.project.render.displayAdapter.MapMouseEvent)
+         */
+        public void mouseDragged( MapMouseEvent e ) {
+            if (dragging){
+                //determining the number of pixels moved in the overview
+                org.eclipse.swt.graphics.Point p = Display.getCurrent().map((Canvas)mapviewer.getViewport(), null, e.x, e.y);
+                int xdiff = p.x - startp.x;
+                int ydiff = p.y - startp.y;
+                //determine the coordinate difference
+                Coordinate oldc = context.pixelToWorld(startp.x, startp.y);
+                Coordinate newc = context.pixelToWorld(p.x, p.y);
+                double xoffset = newc.x - oldc.x;
+                double yoffset = newc.y - oldc.y;
+                
+                //determine the number of pixels on the main map
+                Point p1 = mainmap.getViewportModel().worldToPixel(newc);
+                Point p2 = mainmap.getViewportModel().worldToPixel(oldc);
+                xdiff = p2.x - p1.x;
+                ydiff = p2.y - p1.y;
+                
+                //compute and update bounds
+                final ReferencedEnvelope bounds = mainmap.getViewportModel().getBounds();
+
+                ReferencedEnvelope newbounds = new ReferencedEnvelope(bounds.getMinX() + xoffset,
+                        bounds.getMaxX() + xoffset, bounds.getMinY() + yoffset, bounds.getMaxY()
+                                + yoffset, bounds.getCoordinateReferenceSystem());
+
+                mainmap.getViewportModelInternal().setBounds(newbounds);
+
+                //scroll
+                ((Canvas) mainMapVP).scroll(xdiff, ydiff, 0, 0, mainMapVP.getWidth(), mainMapVP.getHeight(), true);
+                startp = p;                
+            }
+               
+        }
+
+        /**
+         * @see net.refractions.udig.project.ui.tool.AbstractTool#mousePressed(net.refractions.udig.project.render.displayAdapter.MapMouseEvent)
+         */
+        public void mousePressed( MapMouseEvent e ) {
+            Coordinate start = getContext().pixelToWorld(e.x, e.y);
+            if (mainmap.getViewportModelInternal().getBounds().contains(start)){
+                dragging = true;
+                startp = Display.getCurrent().map((Canvas) mapviewer.getViewport(), null, e.x, e.y);
+                mainmap.getViewportModelInternal().setIsBoundsChanging(true);
+            }
+       
+        }
+        /**
+         * @see net.refractions.udig.project.ui.tool.AbstractTool#mouseReleased(net.refractions.udig.project.render.displayAdapter.MapMouseEvent)
+         */
+        public void mouseReleased( MapMouseEvent e ) {
+            if (dragging) {
+                mainmap.getViewportModelInternal().setIsBoundsChanging(false);
+                org.eclipse.swt.graphics.Point p = Display.getCurrent().map((Canvas)mapviewer.getViewport(), null, e.x, e.y);
+                //determine the coordinate difference
+                Coordinate oldc = context.pixelToWorld(startp.x, startp.y);
+                Coordinate newc = context.pixelToWorld(p.x, p.y);
+                double xoffset = newc.x - oldc.x;
+                double yoffset = newc.y - oldc.y;
+                                
+                //compute and update bounds
+                final ReferencedEnvelope bounds = mainmap.getViewportModel().getBounds();
+                ReferencedEnvelope newbounds = new ReferencedEnvelope(bounds.getMinX() + xoffset,
+                        bounds.getMaxX() + xoffset, bounds.getMinY() + yoffset, bounds.getMaxY()
+                                + yoffset, bounds.getCoordinateReferenceSystem());
+                mainmap.getViewportModelInternal().setBounds(newbounds);
+                dragging = false;
+            }
+        }
+    }
 }
