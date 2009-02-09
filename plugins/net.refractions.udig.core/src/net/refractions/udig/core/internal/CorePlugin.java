1b68ab572c80831c8271485994fc3e84d43bbc73
SBorodavkin <SBorodavkin@hg.berlios.de>
2009-02-09 13:49:30 +0200

    Added extra udig-lite dependencies required by udig-psi

--- plugins/net.refractions.udig.core/src/net/refractions/udig/core/internal/CorePlugin.java
+++ plugins/net.refractions.udig.core/src/net/refractions/udig/core/internal/CorePlugin.java
@@ -27,6 +27,7 @@ import org.geotools.referencing.CRS;
 import org.geotools.referencing.ReferencingFactoryFinder;
 import org.geotools.referencing.crs.DefaultGeographicCRS;
 import org.geotools.resources.image.ImageUtilities;
+import org.geotools.util.logging.Logging;
 import org.opengis.geometry.DirectPosition;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import org.opengis.referencing.operation.MathTransform;
@@ -79,7 +80,7 @@ public class CorePlugin extends Plugin {
         super.start(context);
         startGeotools(context);
     }
-	
+   
     /**
      * Returns the system created plugin object
      * 
@@ -164,7 +165,7 @@ public class CorePlugin extends Plugin {
             } catch (MalformedURLException e) {
                 // not a URL, maybe it is a file
                 try {
-                	urls.add( new File(string).toURI().toURL());
+                  urls.add( new File(string).toURI().toURL());
                 } catch (MalformedURLException e1) {
                     // Not a URL, not a File. nothing to do now.
                 }
@@ -241,44 +242,45 @@ public class CorePlugin extends Plugin {
      * @author Jody Garnett
      * @since 1.1.0
      */
-    	public void startGeotools(BundleContext context) throws Exception {
-    	    System.setProperty("org.geotools.referencing.forceXY", "true");
+      public void startGeotools(BundleContext context) throws Exception {
+          System.setProperty("org.geotools.referencing.forceXY", "true");
 
-    		if( Platform.getOS().equals(Platform.OS_WIN32) ){
-    		    try {
-    		        // PNG native support is not very good .. this turns it off
-    		        ImageUtilities.allowNativeCodec("png", ImageReaderSpi.class, false);  //$NON-NLS-1$
-    		    }
-    		    catch (Throwable t){
-    		        // we should not die if JAI is missing; we have a warning for that...
-    		    }
+         if( Platform.getOS().equals(Platform.OS_WIN32) ){
+             try {
+                 // PNG native support is not very good .. this turns it off
+                 ImageUtilities.allowNativeCodec("png", ImageReaderSpi.class, false);  //$NON-NLS-1$
+             }
+             catch (Throwable t){
+                 // we should not die if JAI is missing; we have a warning for that...
+             }
             }
-    	    
-    		// WARNING - the above hints are recommended to us by GeoServer
-    		//           but they cause epsg-wkt not to work because the
-    		//           various wrapper classes trip up over a CRSAuthorityFactory
-    		//           that is not also a OperationAuthorityFactory (I think)
+          
+         // WARNING - the above hints are recommended to us by GeoServer
+         //           but they cause epsg-wkt not to work because the
+         //           various wrapper classes trip up over a CRSAuthorityFactory
+         //           that is not also a OperationAuthorityFactory (I think)
             CoordinateReferenceSystem wgs84 = CRS.decode("EPSG:4326"); // prime the pump - ensure EPSG factory is found
             if( wgs84 == null){
-            	String msg = "Unable to locate EPSG authority for EPSG:4326; consider removing temporary geotools/epsg directory and trying again.";
-            	System.out.println( msg );
-            	//throw new FactoryException(msg);
+               String msg = "Unable to locate EPSG authority for EPSG:4326; consider removing temporary geotools/epsg directory and trying again.";
+               System.out.println( msg );
+               //throw new FactoryException(msg);
             }
-    		Map<Key, Boolean> map = new HashMap<Key, Boolean> ();
-    		//map.put( Hints.FORCE_LONGITUDE_FIRST_AXIS_ORDER, true );
-    	    //map.put( Hints.FORCE_STANDARD_AXIS_DIRECTIONS, true );
-    		//map.put( Hints.FORCE_STANDARD_AXIS_UNITS, true );
-    		map.put( Hints.LENIENT_DATUM_SHIFT, true );		
-    		Hints global = new Hints(map);
-    		GeoTools.init( global );
-    		if( false ){ // how to do debug check with OSGi bundles?
-    		    CRS.main(new String[]{"-dependencies"});
-    		}
-    		ReferencingFactoryFinder.scanForPlugins();
-    		verifyReferencingEpsg();
-    		verifyReferencingOperation();	
-    		
-    	}
+         Map<Key, Boolean> map = new HashMap<Key, Boolean> ();
+         //map.put( Hints.FORCE_LONGITUDE_FIRST_AXIS_ORDER, true );
+          //map.put( Hints.FORCE_STANDARD_AXIS_DIRECTIONS, true );
+         //map.put( Hints.FORCE_STANDARD_AXIS_UNITS, true );
+         map.put( Hints.LENIENT_DATUM_SHIFT, true );     
+         Hints global = new Hints(map);
+         GeoTools.init( global );
+         Logging.GEOTOOLS.setLoggerFactory("org.geotools.util.logging.Log4JLoggerFactory");
+         if( false ){ // how to do debug check with OSGi bundles?
+             CRS.main(new String[]{"-dependencies"});
+         }
+         ReferencingFactoryFinder.scanForPlugins();
+         verifyReferencingEpsg();
+         verifyReferencingOperation(); 
+         
+      }
         /**
          * If this method fails it's because, the epsg jar is either 
          * not available, or not set up to handle math transforms
@@ -287,28 +289,28 @@ public class CorePlugin extends Plugin {
          * @return true if referencing is working and we get the expected result
          * @throws Exception if we cannot even get that far
          */
-    	private void verifyReferencingEpsg() throws Exception {
+      private void verifyReferencingEpsg() throws Exception {
             CoordinateReferenceSystem WGS84 = CRS.decode("EPSG:4326"); // latlong
             CoordinateReferenceSystem BC_ALBERS = CRS.decode("EPSG:3005");
             
             MathTransform transform = CRS.findMathTransform(BC_ALBERS, WGS84 );
             DirectPosition here  = new DirectPosition2D( BC_ALBERS, 1187128, 395268 );
             DirectPosition there = new DirectPosition2D( WGS84, -123.47009173007372,48.54326498732153 );
-            	
+               
             DirectPosition check = transform.transform( here, new GeneralDirectPosition(WGS84) );
             //DirectPosition doubleCheck = transform.inverse().transform( check, new GeneralDirectPosition(BC_ALBERS) );        
 //            if( !check.equals(there)){
-//            	String msg = "Referencing failed to produce expected transformation; check that axis order settings are correct.";
-//            	System.out.println( msg );
-//            	//throw new FactoryException(msg);
+//             String msg = "Referencing failed to produce expected transformation; check that axis order settings are correct.";
+//             System.out.println( msg );
+//             //throw new FactoryException(msg);
 //            }
             double delta = Math.abs(check.getOrdinate(0) - there.getOrdinate(0))+Math.abs(check.getOrdinate(1) - there.getOrdinate(1));
-    		if( delta > 0.0001){
-    			String msg = "Referencing failed to transformation with expected accuracy: Off by "+delta + "\n"+check+"\n"+there;
-    			System.out.println( msg );
-            	//throw new FactoryException(msg);
-            }	
-    	}
+         if( delta > 0.0001){
+            String msg = "Referencing failed to transformation with expected accuracy: Off by "+delta + "\n"+check+"\n"+there;
+            System.out.println( msg );
+               //throw new FactoryException(msg);
+            }  
+      }
         /**
          * If this method fails it's because, the epsg jar is either 
          * not available, or not set up to handle math transforms
@@ -317,8 +319,8 @@ public class CorePlugin extends Plugin {
          * @return true if referencing is working and we get the expected result
          * @throws Exception if we cannot even get that far
          */
-    	private void verifyReferencingOperation() throws Exception {
-    	       // ReferencedEnvelope[-0.24291497975705742 : 0.24291497975711265, -0.5056179775280899 : -0.0]
+      private void verifyReferencingOperation() throws Exception {
+             // ReferencedEnvelope[-0.24291497975705742 : 0.24291497975711265, -0.5056179775280899 : -0.0]
             // ReferencedEnvelope[-0.24291497975705742 : 0.24291497975711265, -0.5056179775280899 : -0.0]
             CoordinateReferenceSystem EPSG4326 = CRS.decode("EPSG:4326");
             ReferencedEnvelope pixelBounds = new ReferencedEnvelope( -0.24291497975705742, 0.24291497975711265, -0.5056179775280899, 0.0, EPSG4326 );
@@ -326,10 +328,10 @@ public class CorePlugin extends Plugin {
             
             ReferencedEnvelope latLong = pixelBounds.transform( WGS84, false );
             if( latLong == null){
-            	String msg = "Unable to transform EPSG:4326 to DefaultGeographicCRS.WGS84";
-            	System.out.println( msg );        		
-            	//throw new FactoryException(msg);
+               String msg = "Unable to transform EPSG:4326 to DefaultGeographicCRS.WGS84";
+               System.out.println( msg );             
+               //throw new FactoryException(msg);
             }
-    	}
-    	
+      }
+      
 }
